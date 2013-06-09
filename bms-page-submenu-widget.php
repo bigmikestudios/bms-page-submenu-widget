81d0572f660641cced03993d647992dc7e70b76f
mike <mikelathrop@Mikes-MacBook-Pro.local>
2013-06-09 13:08:58 -0700

    Include Parent in Menu checkbox

--- bms-page-submenu-widget.php
+++ bms-page-submenu-widget.php
@@ -43,57 +43,63 @@ class Bms_Page_Submenu_Widget extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 		global $post;
-		extract( $args );
-		$title = apply_filters( 'widget_title', $instance['title'] );
-		$depth = intval($instance['depth']);
-		
-		// What depth am I?
-		$parent_id  = $post->post_parent;
-		$my_depth = 0;
-		while ($parent_id > 0) {
-			$page = get_page($parent_id);
-			$parent_id = $page->post_parent;
-			$my_depth++;
-		}
-		
-		// if I have children, then I am the parent.
-		$children = get_pages(array('child_of' => $post->ID));
-        $is_parent = (count( $children ) != 0 ) ? true : false;
-		$is_child = ($post->post_parent) ? true : false;
-		$siblings = get_pages(array('child_of' => $post->post_parent));
-		$include = '';
-		
-		// what goes in the menu?
-		if( $is_parent && ($my_depth == $depth) ) { 
-			foreach($children as $child) {
-				$include .= $child->ID. ", ";
-			}
-		} else if ( count($siblings) != 0 && $my_depth == ($depth+1) ) {
-			foreach ($siblings as $sib) {
-				$include .= $sib->ID. ", ";
-			}
-		}
-		if ( $include != '' ) {
-			// get the menu
-			$args = array(
-			  'include' => $include,
-			  'echo' => false,
-			);
-			$subpage_list = wp_page_menu($args);
-	
-			// apply variables
-			$title = str_replace('@@title@@', get_the_title($menu_parent_id), $title);
+		if (isset($post->ID)) {
+			extract( $args );
+			$title = apply_filters( 'widget_title', $instance['title'] );
+			$depth = intval($instance['depth']);
+			$include_parent = $instance['include_parent'];
 			
+			// What depth am I?
+			$parent_id  = isset($post->post_parent) ? $post->post_parent : NULL;
+			$my_depth = 0;
+			while ($parent_id > 0) {
+				$page = get_page($parent_id);
+				$parent_id = $page->post_parent;
+				$my_depth++;
+			}
 			
-			if ($subpage_list) {
-				echo $before_widget;
-				if ( ! empty( $title ) )
-					echo $before_title . $title . $after_title;
+			// if I have children, then I am the parent.
+			$children = get_pages(array('child_of' => $post->ID));
+			$is_parent = (count( $children ) != 0 ) ? true : false;
+			$is_child = ($post->post_parent) ? true : false;
+			$siblings = get_pages(array('child_of' => $post->post_parent));
+			$include = array();
+			
+			// what goes in the menu?
+			if( $is_parent && ($my_depth == $depth) ) { 
+				foreach($children as $child) {
+					$include[] = $child->ID;
+				}
+				if ($include_parent == 'checked') $include[] = $post->ID;
+			} else if ( count($siblings) != 0 && $my_depth == ($depth+1) ) {
+				foreach ($siblings as $sib) {
+					$include[] = $sib->ID;
+				}
+				if ($include_parent == 'checked') $include[] = $post->post_parent;
+			}
+			
+			if ( $include != '' ) {
+				// get the menu
+				$args = array(
+				  'include' => $include,
+				  'echo' => false,
+				  'posts_per_page' => 999,
+				);
+				$subpage_list = wp_page_menu($args);
 		
-				?>
-					<?php echo($subpage_list); ?>
-				<?php
-				echo $after_widget;
+				// apply variables
+				$title = str_replace('@@title@@', get_the_title($parent_id), $title);
+				
+				if ($subpage_list) {
+					echo $before_widget;
+					if ( ! empty( $title ) )
+						echo $before_title . $title . $after_title;
+			
+					?>
+						<?php echo($subpage_list); ?>
+					<?php
+					echo $after_widget;
+				}
 			}
 		}
 	}
@@ -112,6 +118,7 @@ class Bms_Page_Submenu_Widget extends WP_Widget {
 		$instance = array();
 		$instance['title'] = strip_tags( $new_instance['title'] );
 		$instance['depth'] = intval( $new_instance['depth'] );
+		if ($new_instance['include_parent']=='checked') $instance['include_parent'] = 'checked';
 		return $instance;
 	}
 
@@ -125,21 +132,30 @@ class Bms_Page_Submenu_Widget extends WP_Widget {
 	public function form( $instance ) {
 		if ( isset( $instance[ 'title' ] ) ) {
 			$title = $instance[ 'title' ];
-		}
-		else {
+		} else {
 			$title = __( 'New title', 'text_domain' );
 		}
+		
 		if ( isset( $instance[ 'depth' ] ) ) {
 			$depth = $instance[ 'depth' ];
-		}
-		else {
+		} else {
 			$depth = 0;
+		}
+		if ( isset( $instance[ 'include_parent' ] ) ) {
+			$include_parent = $instance[ 'include_parent' ];
+			$include_parent = ($include_parent=='checked') ? 'checked' : '';
+		} else {
+			$include_parent = '';
 		}
 		
 		?>
 		<p>
 		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
 		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /><br /><small>Use <em>@@title@@</em> to include the title of the parent page</small>
+		</p>
+        <p>
+		<label for="<?php echo $this->get_field_id( 'include_parent' ); ?>"><?php _e( 'Include parent page in menu?' ); ?></label> 
+		<input id="<?php echo $this->get_field_id( 'include_parent' ); ?>" name="<?php echo $this->get_field_name( 'include_parent' ); ?>" type="checkbox" value="checked" <?php echo $include_parent; ?> />
 		</p>
         <p>
 		<label for="<?php echo $this->get_field_id( 'depth' ); ?>"><?php _e( 'Depth:' ); ?></label> 
